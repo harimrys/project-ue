3dfa45da2e7f6bfdbbe5ba5d85d28a4292a68596
cristinaramirezdiaz <cristinaramirezdiaz02@gmail.com>
2024-09-11 12:33:38 +0200

    column names

--- functions.py
+++ functions.py
@@ -13,6 +13,8 @@ def change_value(df):
     gender_map = {"F:Females": "Females", "M:Males": "Males", "T:Total": "Total", "F": "Females", "M": "Males", "T": "Total"}
     df["sex"] = df["sex"].map(gender_map)
 
+    age_map = {"Y15-29": "15-29", "Y15-29:From 15 to 29 years": "15-29"}
+    df["age"] = df["age"].map(age_map)
 
     return df
 
@@ -35,6 +37,20 @@ def drop_columns_unemployment(df): # Elimina las columnas de la tabla desempleo 
 
     return df
 
+def column_names_education(df):
+    df.rename(columns={"isced11": "level_education"}, inplace=True)
+    df.rename(columns={"geo": "country"}, inplace=True)
+    df.rename(columns={"time_period": "year"}, inplace=True)
+    df.rename(columns={"obs_value": "value_education"}, inplace=True)
+    return df
+
+def column_names_unemployment(df):
+    df.rename(columns={"isced11": "level_education"}, inplace=True)
+    df.rename(columns={"geo": "country"}, inplace=True)
+    df.rename(columns={"time_period": "year"}, inplace=True)
+    df.rename(columns={"obs_value": "value_unemployment"}, inplace=True)
+    return df
+
 def change_name_country_education(df):
     geo_map = {"AT:Austria" : "Austria", "BA:Bosnia and Herzegovina": "Bosnia and Herzegovina", "BE:Belgium": "Belgium",
                "BG:Bulgaria": "Bulgaria", "CH:Switzerland": "Switzerland", "CY:Cyprus": "Cyprus", "CZ:Czechia": "Czechia",
@@ -45,7 +61,7 @@ def change_name_country_education(df):
                "NO:Norway": "Norway", "PL:Poland": "Poland", "PT:Portugal": "Portugal", "RO:Romania": "Romania", "RS:Serbia": "Serbia",
                "SE:Sweden": "Sweden", "SI:Slovenia": "Slovenia", "SK:Slovakia": "Slovakia", "TR:Türkiye": "Türkiye", 
                "UK:United Kingdom": "United Kingdom"}
-    df["geo"] = df["geo"].map(geo_map)
+    df["country"] = df["country"].map(geo_map)
     return df
 
 def change_name_country_unemployment(df):
@@ -57,5 +73,5 @@ def change_name_country_unemployment(df):
                "NO": "Norway", "PL": "Poland", "PT": "Portugal", "RO": "Romania", "RS": "Serbia",
                "SE": "Sweden", "SI": "Slovenia", "SK": "Slovakia", "TR": "Türkiye", 
                "UK": "United Kingdom"}
-    df["geo"] = df["geo"].map(geo_map)
+    df["country"] = df["country"].map(geo_map)
     return df
